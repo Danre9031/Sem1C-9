e4ad0999652fb090d29b109764cc1c1864bd4701
Mihail <Dante9041@mail.ru>
2022-07-11 16:45:58 +0300

    add DZ2

---
+++ DZ2 66/Program.cs
@@ -0,0 +1,30 @@
+﻿int firstArgument = Input("M: ");
+int secondArgument = Input("N: ");
+int summ=0;
+
+if(firstArgument<secondArgument) 
+    SummNaturalNumber(firstArgument,secondArgument,summ);
+else 
+    SummNaturalNumber(secondArgument,firstArgument,summ);
+
+
+void SummNaturalNumber(int m,int n,int summ)
+{
+    if(m == n)
+    {
+        summ+=m;
+        Console.Write($"Сумма={summ}");
+    }
+    else
+    {
+        summ+=m+n;
+        SummNaturalNumber(++m,--n,summ);
+    }
+    
+}
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
