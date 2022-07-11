75845720e1efa6d4c5730ff60bdbdae989c6315b
Mihail <Dante9041@mail.ru>
2022-07-11 16:45:21 +0300

    add DZ1

---
+++ DZ1 64/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = Input("Введите число: ");
+int temp=1;
+
+Console.WriteLine(NaturalNumber(number,temp));
+
+int NaturalNumber(int numberOne,int numberTwo)
+{
+    if(numberOne == numberTwo)
+        return numberTwo;
+    else
+        Console.Write($"{NaturalNumber(numberOne,numberTwo+1)}, ");
+    return numberTwo;
+}
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
